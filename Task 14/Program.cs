711635b35f5a4c9495d1a750bf4609e5a125a77e
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-26 23:12:04 +0300

    Черновик задачи 14

---
+++ Task 14/Program.cs
@@ -0,0 +1,18 @@
+﻿// 14. Программа, которая задаёт массив положительных трехзначных чисел
+// и показывает количество четных чисел в массиве.
+
+int EnterInfomation(string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+int LengthArray = EnterInfomation(" Введите длину массива : ");
+int [] RandomArray=new int [LengthArray];
+for (int i=0;i<RandomArray.Length;i++)
+{
+RandomArray[i]=new Random().Next(100,999);
+    if (RandomArray[i]%2==0)
+{
+System.Console.Write(RandomArray[i]+" ");
+}
+}
