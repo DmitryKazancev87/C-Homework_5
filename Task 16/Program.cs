40c94ebebb0d8d7c34620a5f5a111a08d3a63a38
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-28 22:51:47 +0300

    Черновик задачи 16

---
+++ Task 16/Program.cs
@@ -0,0 +1,29 @@
+﻿// 16. Программа, которая задаёт массив вещественных чисел
+// и находит разницу между максимальным и минимальным элементом массива.
+
+int EnterInfomation(string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+int LengthArray = EnterInfomation(" Введите длину массива : ");
+double [] RandomArray=new double [LengthArray];
+for (int i=0;i<RandomArray.Length;i++)
+{
+RandomArray[i]=new Random().Next(1,1000);
+System.Console.Write(RandomArray[i]+" ");
+}
+double min=Int32.MaxValue;
+double max=Int32.MinValue;
+for (int j=0;j<RandomArray.Length; j++)
+{
+   if (RandomArray[j]> max)
+   {
+    max= RandomArray [j];
+   }
+   if (RandomArray[j]<min)
+   {
+    min=RandomArray[j];
+   }   
+}
+System.Console.Write($": всего {RandomArray.Length} чисел, максимальное значение = {max}, минимальное значение = {min}");
