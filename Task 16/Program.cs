0a75d80355e2872867750ce29b9209bba209b9a0
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-28 23:52:35 +0300

    Решение задачи 16

--- Task 16/Program.cs
+++ Task 16/Program.cs
@@ -3,28 +3,28 @@
 
 int EnterInfomation(string message)
 {
-Console.Write(message);
-return int.Parse(Console.ReadLine()!);
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
 }
 int LengthArray = EnterInfomation(" Введите длину массива : ");
-double [] RandomArray=new double [LengthArray];
-for (int i=0;i<RandomArray.Length;i++)
+double[] RandomArray = new double[LengthArray];
+for (int i = 0; i < RandomArray.Length; i++)
 {
-RandomArray[i]=new Random().Next(1,1000);
-System.Console.Write(RandomArray[i]+" ");
+    RandomArray[i] = new Random().Next(1, 1000);
+    System.Console.Write(RandomArray[i] + " ");
 }
-double min=Int32.MaxValue;
-double max=Int32.MinValue;
-for (int j=0;j<RandomArray.Length; j++)
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+for (int j = 0; j < RandomArray.Length; j++)
 {
-   if (RandomArray[j]> max)
-   {
-    max= RandomArray [j];
-   }
-   if (RandomArray[j]<min)
-   {
-    min=RandomArray[j];
-   }   
+    if (RandomArray[j] > max)
+    {
+        max = RandomArray[j];
+    }
+    if (RandomArray[j] < min)
+    {
+        min = RandomArray[j];
+    }
 }
 System.Console.WriteLine($": всего {RandomArray.Length} чисел(а), максимальное значение = {max}, минимальное значение = {min}.");
-System.Console.WriteLine($" Разница между максимальным и минимальным элементом массива = {max-min}.");
+System.Console.WriteLine($" Разница между максимальным и минимальным элементом массива = {max - min}.");
